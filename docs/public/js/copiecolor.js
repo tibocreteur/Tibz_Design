39eb61331664720e2fa0a4172d13f413275ec92c
tibocreteur <tibocreteur@gmail.com>
2025-01-10 16:31:35 +0100

    dswfxcgh

--- docs/public/js/copiecolor.js
+++ docs/public/js/copiecolor.js
@@ -1,8 +1,7 @@
 // Sélectionner toutes les couleurs
 const colors = document.querySelectorAll('.color');
-const copiedMessage = document.querySelector('.copied-message');
-
-// Fonction de copie et affichage du message
+const copiedMessage = document.querySelector('.copied-message')
+;
 colors.forEach(color => {
     color.addEventListener('click', async () => {
         const colorCode = color.getAttribute('data-text'); // Récupère le code couleur (data-text)
@@ -10,11 +9,6 @@ colors.forEach(color => {
         try {
             // Utilise l'API moderne pour copier du texte
             await navigator.clipboard.writeText(colorCode);
-
-            // Ajoute une vibration (200ms)
-            if ("vibrate" in navigator) {
-                navigator.vibrate(200); // Vibrer pendant 200ms
-            }
 
             // Affiche le message "copié"
             copiedMessage.style.display = 'block';
@@ -30,6 +24,8 @@ colors.forEach(color => {
         }
     });
 });
+
+
 
 
 
